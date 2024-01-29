a38cac7395feee66801ed17247a84166bdb0465c
K S Subramanya Char <123807459+s16exe@users.noreply.github.com>
2024-01-29 19:48:38 +0530

    Update MicroPollutantsActivity.kt

--- app/src/main/java/com/s16/febraury/Screens/Micro/MicroPollutantsActivity.kt
+++ app/src/main/java/com/s16/febraury/Screens/Micro/MicroPollutantsActivity.kt
@@ -6,29 +6,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsTopHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -50,7 +44,7 @@ fun MicroPollutantsActivity(navController: NavHostController) {
     }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Text(
             text = "POLLUTANTS",
@@ -63,7 +57,6 @@ fun MicroPollutantsActivity(navController: NavHostController) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         )
-
         {
             Box(
                 contentAlignment = Alignment.Center,
@@ -74,8 +67,8 @@ fun MicroPollutantsActivity(navController: NavHostController) {
             )
             {
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Top,
+                    modifier = Modifier.padding(5.dp,20.dp),
+                    verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
 
@@ -258,7 +251,8 @@ fun MicroPollutantsActivity(navController: NavHostController) {
             modifier = Modifier
                 .width(250.dp)
                 .padding(16.dp)
-                .clip(RoundedCornerShape(50.dp))
+                .clip(RoundedCornerShape(50.dp)),
+            contentAlignment = Alignment.Center
         ) {
             Button(
                 onClick = { navController.popBackStack() },
